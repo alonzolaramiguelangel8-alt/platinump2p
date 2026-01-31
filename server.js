92ce11e54b82522be9769e9f92ca095ee937f8fe
alonzolaramiguelangel8-alt <alonzolaramiguelangel8@gmail.com>
2026-01-31 09:12:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ const pool = new Pool({
 const initDB = async () => {
     try {
         await pool.query(`
-        await pool.query('DROP TABLE IF EXISTS users CASCADE;');
             CREATE TABLE IF NOT EXISTS users (
                 id SERIAL PRIMARY KEY,
                 username TEXT UNIQUE,
